1c2939a7745bcc57a3277c6a8fd4beb2dfd0fdf9
조민현 <cho.minhyun@cashwalk.io>
2023-07-27 15:25:38 +0900

    Update Package.swift for SPM Test

--- Package.swift
+++ Package.swift
@@ -1,1 +1,16 @@
+// swift-tools-version:5.3
+import PackageDescription
 
+let package = Package(
+    name: "Momento_iOS",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(name: "Momento_iOS", targets:["Momento_iOS"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "Momento_iOS", path: "Momento_iOS.xcframework")
+    ]
+)
